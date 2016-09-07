452566bf55a0124f3ce75ef5946f11019efc57b5
Jennifer Brown <j.a.brown.5@gmail.com>
2016-09-07 12:33:08 -0500

    popups for matches added

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,13 @@
-//console.log("JS file is connected to HTML! Woo!")
-
+// Adding King and Queen cards
 var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardThree) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, Try again.");
+}
